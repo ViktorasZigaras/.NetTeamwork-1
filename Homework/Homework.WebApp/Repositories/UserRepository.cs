86779fcd576c3a806288a93cc28e31ea58fba555
Tomas Daugela <todaprojects@gmail.com>
2021-01-14 17:28:46 +0200

    Added Models, Data, Repositories

---
+++ Homework/Homework.WebApp/Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Homework.WebApp.Interfaces;
+using Homework.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homework.WebApp.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DbContext _context;
+
+        public UserRepository(DbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<List<User>> GetAllIUsersAsync()
+        {
+            return await _context.Set<User>().ToListAsync();
+        }
+
+        public async Task<User> GetUserAsync(int id)
+        {
+            return await _context.Set<User>().FindAsync(id);
+        }
+
+        public async Task AddItemAsync(User user)
+        {
+            _context.Set<User>().Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EditUserAsync(int id, User user)
+        {
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteUserAsync(int id)
+        {
+            var user = await GetUserAsync(id);
+            _context.Set<User>().Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
